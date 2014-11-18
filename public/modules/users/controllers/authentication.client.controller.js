7030c9c4d8f670243d51d51c68b383c0e74cc2ca
Chris <jester@jester-P150HMx.(none)>
2014-11-17 23:02:38 -0800

    modified signup.client.view to have radio buttons to select artist or business form. Authentication.client.controller modified to allow radio button functionality.

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -3,9 +3,16 @@
 angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication',
 	function($scope, $http, $location, Authentication) {
 		$scope.authentication = Authentication;
+		$scope.user = '';
 
 		// If user is signed in then redirect back home
 		if ($scope.authentication.user) $location.path('/');
+
+		$scope.isShown = function(user){
+			return user === $scope.user;
+		};
+		// Default form shown is artist
+		$scope.user = 'Artist';
 
 		$scope.signup = function() {
 			//creating a change in the file.
